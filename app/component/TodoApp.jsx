476557d9336f4fb485157d288ddc22fba888723d
ajisah87 <sahajit119@gmail.com>
2023-11-28 21:57:27 +0530

    initial commit

---
+++ app/component/TodoApp.jsx
@@ -0,0 +1,166 @@
+"use client";
+import { useEffect, useState } from "react";
+import CanvasBgAnimation from "./CanvasBgAnimation";
+import { FaPlus } from "react-icons/fa6";
+import { motion, AnimatePresence } from "framer-motion";
+import TodoModal from "./TodoModal";
+import { MdDelete, MdCheckBoxOutlineBlank } from "react-icons/md";
+import { FaEdit, FaRegCheckSquare } from "react-icons/fa";
+
+const TodoApp = () => {
+
+  const initialDemoTodos = [
+    { title: "work", desc: "check email", complition: false },
+    { title: "works", desc: "check email", complition: false },
+    { title: "works", desc: "check emails", complition: false },
+  ];
+
+  const [title, setTitle] = useState("");
+  const [desc, setdesc] = useState("");
+  const [IsModalOpen, setIsModalOpen] = useState(false);
+  const [mainTask, setMainTask] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [indexForEdit, setIndexForEdit] = useState(null);
+  const [viewAll, setViewAll] = useState(false)
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      let todos = localStorage.getItem("todos");
+      try {
+        let savedTodo = JSON.parse(todos);
+        if (Array.isArray(savedTodo)) {
+          setMainTask(savedTodo.length > 0 ? savedTodo : []);
+        } else {
+          setMainTask([]);
+        }
+      } catch (error) {
+        console.error("Error parsing todos from localStorage:", error);
+        setMainTask([]);
+      } finally {
+        setLoading(false);
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(mainTask));
+  }, [mainTask]);
+
+  const deleteTaskHandler = (index) => {
+    let copyTask = [...mainTask];
+    copyTask.splice(index, 1);
+    setMainTask(copyTask);
+  };
+
+  const handleEditTodo = (index) => {
+    const todoToEdit = mainTask[index];
+    setIndexForEdit(index);
+
+    setTitle(todoToEdit.title);
+    setdesc(todoToEdit.desc);
+
+    setIsModalOpen(true);
+  };
+
+  const handleTodoComplition = (index) => {
+    const todoToComplition = [...mainTask];
+    todoToComplition[index].complition = !todoToComplition[index].complition;
+    setMainTask(todoToComplition);
+  };
+
+  return (
+    <div>
+      <h2 className="text-center text-5xl font-bold my-8">Todo List</h2>
+      <div className="w-[min(1200px,100%-2rem)] mx-auto bg-slate-800 min-h-[500px] flex flex-col justify-center py-10">
+        <button
+          onClick={() => setIsModalOpen((prev) => !prev)}
+          className="mb-3 p-4 w-1/2 h-20 mx-auto relative rounded-md overflow-hidden"
+        >
+          <span className="absolute inset-0 ">
+            <CanvasBgAnimation />
+          </span>
+          <span className="absolute inset-0 backdrop-blur-2xl bg-white/20 z-20  grid place-content-center text-white">
+            <FaPlus className="text-5xl text-slate-700" />
+          </span>
+        </button>
+
+        {loading ? (
+          <div className="text-3xl font-semibold text-center text-white">loading..</div>
+        ) : (
+          <AnimatePresence initial={false}>
+            {mainTask.slice(0, viewAll ? mainTask.length : 3).map((todo, i) => (
+              <motion.div
+              initial={{opacity: 0, height: 0, scale: .8}}
+              animate={{opacity: 1, height: 'auto', scale: 1}}
+              exit={{opacity: 0, height: 0, scale: .8}}
+              transition={{opacity: {duration: .2}}}
+              key={`${todo.title}-${todo.desc}`}
+              >
+                <div className="p-4  rounded-md mb-3 w-1/2 mx-auto gap-4 bg-amber-200 flex justify-between items-center">
+                  <div className=" w-[90%]  break-words  overflow-x-scroll">
+                    <div
+                      className={`text-xl font-black text-slate-800  ${
+                        todo.complition && "line-through opacity-50"
+                      }`}
+                    >
+                      {todo.title}
+                    </div>
+                    <div
+                      className={`text-lg font-normal text-slate-800 ${todo.complition && "line-through  opacity-50"}`}
+                    >
+                      {todo.desc}
+                    </div>
+                  </div>
+                  <div className="flex justify-center items-center gap-3">
+                    <button onClick={() => deleteTaskHandler(i)}>
+                      <MdDelete className="text-2xl" />
+                    </button>
+                    <button
+                      className={`${
+                        todo.complition && "opacity-50 pointer-events-none"
+                      }`}
+                    >
+                      <FaEdit
+                        onClick={() => handleEditTodo(i)}
+                        className="text-2xl"
+                      />
+                    </button>
+                    <button onClick={() => handleTodoComplition(i)}>
+                      {todo.complition ? (
+                        <FaRegCheckSquare className="text-2xl" />
+                      ) : (
+                        <MdCheckBoxOutlineBlank className="text-2xl" />
+                      )}
+                    </button>
+                  </div>
+                </div>
+              </motion.div>
+            ))}
+          </AnimatePresence>
+        )}
+      {
+        mainTask.length > 3 && (
+          <button className="text-white py-2 px-3 rounded-md mt-2 bg-blue-500 hover:bg-blue-600 transition-colors w-fit mx-auto" onClick={() => setViewAll(prev => !prev)}>{viewAll ? 'show less': 'view all'}</button>
+        )
+      }
+      </div>
+      <AnimatePresence>
+        {IsModalOpen && (
+          <TodoModal
+            setIsModalOpen={setIsModalOpen}
+            setMainTask={setMainTask}
+            mainTask={mainTask}
+            title={title}
+            setTitle={setTitle}
+            desc={desc}
+            setdesc={setdesc}
+            indexForEdit={indexForEdit}
+            setIndexForEdit={setIndexForEdit}
+          />
+        )}
+      </AnimatePresence>
+    </div>
+  )
+}
+
+export default TodoApp;
--- app/page.js
+++ app/page.js
@@ -1,112 +1,16 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import TodoApp from "./component/TodoApp"
+import { Toaster } from "sonner";
 
-function page() {
-  const [title, setTitle] = useState("");
-  const [desc, setdesc] = useState("");
-  const [loading, setLoading] = useState(true);
-
-  const [mainTask, setMainTask] = useState([]);
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      let todos = localStorage.getItem("todos");
-      try {
-        let savedTodo = JSON.parse(todos);
-        if (Array.isArray(savedTodo)) {
-          setMainTask(savedTodo);
-          setLoading(false)
-        } else {
-          setMainTask([]);
-        }
-      } catch (error) {
-        console.error("Error parsing todos from localStorage:", error);
-        setMainTask([]);
-      }
-    }
-  }, []);
-  
-
-  useEffect(()=>{
-    localStorage.setItem("todos",JSON.stringify(mainTask))
-  },[mainTask])
-  
-  const submitHandler = (e) => {
-    e.preventDefault();
-    if (!title || !desc  ) return 
-    setMainTask([...mainTask, { title, desc }]);
-    setTitle("");
-    setdesc("");
-    
-  };
-  const deleteTaskHandler = (i) => {
-    let copyTask = [...mainTask];
-    copyTask.splice(i, 1);
-    setMainTask(copyTask);
-  };
-  let renderTask = <h2 className="text-center"> {loading ? 'Loading..' : 'No Task Available'}</h2>;
-  if (mainTask.length > 0) {
-    renderTask = mainTask.map((t, i) => {
-      return (
-        <ul key={i} className="flex items-center justify-between">
-          <li className="">
-            <div className="flex items-center justify-between mb-5 w-2/3">
-              <h4 className="text-2xl font-semibold ">{t.title}</h4>
-              <h5 className="text-lg font-medium  ">{t.desc}</h5>
-            </div>
-            <button
-              onClick={() => {
-                deleteTaskHandler(i);
-              }}
-              className="bg-yellow-300 px-4 py-2 text-red-500 mb-6 rounded"
-            >
-              Delete
-            </button>
-          </li>
-        </ul>
-      );
-    });
-  }
-
+const page = () => {
+ 
   return (
-    <>
-      <h1 className="bg-red-500 w-full text-yellow-100 p-5 text-center font-bold text-4xl">
-        Real ToDo List
-      </h1>
-      <form onSubmit={submitHandler} className="mb-4">
-        <input
-          className="text-2xl bg-yellow-100 border-red-500 text-red-800 outline-none border-4 m-5 py-2 px-4"
-          placeholder="Topic Kya Rakhu Ab"
-          type="text"
-          onChange={(e) => {
-            setTitle(e.target.value);
-          }}
-          value={title}
-        />
-        <input
-          className="text-2xl bg-yellow-100 border-red-500 text-red-800 border-4 outline-none m-5 py-2 px-4"
-          placeholder="Kya Lena Hai Idhar Likh Lo"
-          type="text"
-          value={desc}
-          onChange={(e) => {
-            setdesc(e.target.value);
-          }}
-        />
-        <button
-          className="bg-red-500 text-yellow-100 font-bold rounded m-5 py-2 px-4 text-2xl "
-         
-        >
-          Add Task
-        </button>
-      </form>
-      <div className="bg-red-500 p-8">
-        <ul className="text-yellow-100 font-bold text-2xl">
-          <li>{renderTask}</li>
-          </ul>
-      </div>
-    </>
-  );
+    <div>
+      <TodoApp />
+      <Toaster richColors position="top-center" />
+    </div>
+  )
 }
 
-export default page;
+export default page
